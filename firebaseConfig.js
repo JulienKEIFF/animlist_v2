c9a8ea8282edb0497501a30e87d2c200cdd498bd
julien <j.keiff@hotmail.fr>
2020-05-10 16:57:17 +0200

    fix(fbConfig):Change info on env variable

---
+++ firebaseConfig.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// firebase init goes here
+const config = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DBURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.BUCKET,
+    messagingSenderId: process.env.MESSAGING
+}
+
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+const currentUser = auth.currentUser
+
+// firebase collections
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+export {
+    db,
+    auth,
+    currentUser,
+    usersCollection,
+    postsCollection,
+    commentsCollection,
+    likesCollection
+}
